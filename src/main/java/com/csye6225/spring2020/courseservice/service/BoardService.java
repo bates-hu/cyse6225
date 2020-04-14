50e60130ba107d2c91981a1f769d5178c90a8a2f
bates-hu <hbc0204@foxmail.com>
2020-04-14 00:41:31 -0700

    check course when add board

--- src/main/java/com/csye6225/spring2020/courseservice/service/BoardService.java
+++ src/main/java/com/csye6225/spring2020/courseservice/service/BoardService.java
@@ -1,9 +1,25 @@
 package com.csye6225.spring2020.courseservice.service;
 
 import com.csye6225.spring2020.courseservice.datamodel.Board;
+import com.csye6225.spring2020.courseservice.utils.ResponseWrapper;
 
 public class BoardService extends RestService<Board>{
+    private CoursesService coursesService;
     public BoardService() {
         super(Board.class);
+        this.coursesService = new CoursesService();
+    }
+
+    @Override
+    public ResponseWrapper addWithGeneratedId(Board model) {
+        if (model.getCourseId() == null) {
+            return ResponseWrapper.ofErr(10, "no course Id");
+        }
+
+        if (coursesService.get(model.getCourseId()).getBody() != null) {
+            return ResponseWrapper.ofErr(11, "course was created");
+        }
+
+        return super.addWithGeneratedId(model);
     }
 }
